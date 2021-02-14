eaa33bf13e027fb365f73ec65e758e442a963298
Avd90 <avdeshacs@gmail.com>
2021-02-14 14:58:08 -0800

    added some changes in index html files and App.js files

--- src/Componant/Layout/Navbar.js
+++ src/Componant/Layout/Navbar.js
@@ -12,15 +12,15 @@ const Navbar = () =>{
                     className="navbar-toggler" 
                     type="button" 
                     data-toggle="collapse" 
-                    data-target="#navbarSupportedContant" 
-                    aria-controLs="navbarSupportedContant" 
+                    data-target="#navbarNav" 
+                    aria-controls="navbarNav" 
                     aria-expanded="false" 
                     aria-label="Toggle navigation"
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link" exact to="/">Home</Link>
                         </li>
